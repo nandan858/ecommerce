ce8358ad4bfa8f9e924bb15c1c94687a4e8f534a
nandan858 <nandanpk90@gmail.com>
2022-11-16 11:12:13 +0530

    Added banner in Sanity

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,39 @@
 import React from 'react';
+import {client} from '../lib/client';
 
 //components
 import Product from '../components/Product';
 import FooterBanner from '../components/FooterBanner';
 import Herobanner from '../components/HeroBanner';
 
-const Home = () => {
+const Home = ({products, bannerData}) => {
   return (
    <>
-   <Herobanner/>
+   <Herobanner heroBanner={bannerData.length && bannerData[0]}/>
 
    <div className='products-heading'>
-    <h2>Best Selling Product</h2>
+    <h2>Best Seller Product</h2>
     <p>Speakers of many variations</p>
    </div>
 
    <div className='products-container'>
-    {['Product 1','Product 2'].map((product) => product)}
+    {products?.map((product) => product.name)}
    </div>
 
    <FooterBanner/>
    </>
   )
 }
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
 
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+
+return{
+    props: {products, bannerData}
+}
+}
 export default Home;
